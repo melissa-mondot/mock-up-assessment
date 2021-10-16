24dc624db09317a80a3fbdd46afe233bc6845f37
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-10-16 16:57:37 -0700

    Add animation for icons

--- mockup/src/components/molecules/Card/IconCard.js
+++ mockup/src/components/molecules/Card/IconCard.js
@@ -4,14 +4,17 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 // icons
 
-// TODO: take in vars with data and map images for reusability
 // TODO: subtle animation for icons
 
 const IconCard = ({ icons }) => {
   console.log(icons);
   const iconTiles = icons.map((key, icon) => (
     <Col className="my-3" key={key}>
-      <Image src={icons[icon][0]} alt={`Illustration of ${icons[icon][1]}`} />
+      <Image
+        className="animated-icon"
+        src={icons[icon][0]}
+        alt={`Illustration of ${icons[icon][1]}`}
+      />
     </Col>
   ));
   return <Row>{iconTiles}</Row>;
